d3b8aa3ef703218847b713edfbfa813718670a53
eddythesixthofsevens <eyadwhassan@gmail.com>
2025-10-28 19:10:42 +0000

    fixed???

--- npc_gen.py
+++ npc_gen.py
@@ -6,14 +6,15 @@ hp = random.randint(1,101)
 defense = random.randint(1,101)
 atk = random.randint(1,101)
 spd = random.randint(1,101)
-height = random.uniform(0, 10.0)
+height = int(random.uniform(0, 10.0))
 amount_of_npcs = (int(input("Enter how many NPCs you want: ")) + 1)
 
 for amount in range(0, amount_of_npcs):
     npc_name = random.choice(name) + "'s"
+    atk = random.randint(1,101)
     npc = f" {npc_name} HP is: {hp}\n {npc_name} DEF is: {defense}\n {npc_name} ATK is: {atk}\n {npc_name} SPD is: {spd}\n Height is: {height} meters"
-    if npc_name == "kaiser" or npc_name == "lavinho" or npc_name == "gojo":
-        print(npc + "\nThis one has aura.")
+    if atk < 50:
+        print(npc + "\n This one's weak.")
     else:
         print(npc)
     print()
