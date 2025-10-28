49bf6cb4bbf97c703453bde3ddd9e936e5c47c06
eddythesixthofsevens <eyadwhassan@gmail.com>
2025-10-28 18:25:15 +0000

    challenge???

--- npc_gen.py
+++ npc_gen.py
@@ -1,15 +1,19 @@
 # NPC GENERATOR (SO TUFF TRUSTTT)
 
 import random
-name = ["habibi", "fitch", "jimmy", "dora", "haleeb", "forker", "eclair", "kavin", "nemo", "obcd", "horse", "turk", "area (length x width)", "angulo", "utensil", "kaiser", "lavinho"]
-npc_name = random.choice(name) + "'s"
+name = ["slipper", "fitch", "jimmy", "dora", "bind", "forker", "eclair", "kavin", "nemo", "acrd", "horse", "turk", "area (length x width)", "angulo", "utensil", "kaiser", "lavinho", "gojo"]
 hp = random.randint(1,101)
 defense = random.randint(1,101)
 atk = random.randint(1,101)
 spd = random.randint(1,101)
-amount_of_npcs = int(input("Enter how many NPCs you want: "))
+height = random.uniform(0, 10.0)
+amount_of_npcs = (int(input("Enter how many NPCs you want: ")) + 1)
 
-npc = f" {npc_name} HP is: {hp}\n {npc_name} DEF is: {defense}\n {npc_name} ATK is: {atk} \n {npc_name} SPD is: {spd}"
 for amount in range(0, amount_of_npcs):
-    print(npc)
+    npc_name = random.choice(name) + "'s"
+    npc = f" {npc_name} HP is: {hp}\n {npc_name} DEF is: {defense}\n {npc_name} ATK is: {atk}\n {npc_name} SPD is: {spd}\n Height is: {height} meters"
+    if npc_name == "kaiser" or npc_name == "lavinho" or npc_name == "gojo":
+        print(npc + "\nThis one has aura.")
+    else:
+        print(npc)
     print()
